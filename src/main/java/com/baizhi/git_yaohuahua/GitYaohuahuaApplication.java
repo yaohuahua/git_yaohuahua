410d66a769bbae0c90651a41580396749cb8b6e6
huahua <yaohuahuaya@163.com>
2020-11-30 21:34:33 +0800

    第一次

---
+++ src/main/java/com/baizhi/git_yaohuahua/GitYaohuahuaApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.git_yaohuahua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitYaohuahuaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitYaohuahuaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baizhi/git_yaohuahua/GitYaohuahuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.git_yaohuahua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitYaohuahuaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
